43f3fcfb432b6cbc68e54900ea145b411d025af6
LockerCompany <lockcompanygroup@gmail.com>
2017-08-06 09:50:47 -0400

    constraint for no_go

--- kenken_csp.py
+++ kenken_csp.py
@@ -17,7 +17,6 @@ def kenken_csp_model(course_list):
     #list of timeslots e.g.["M10","Tu10","W16"]
     no_go = course_list[course_num+1]
 
-
     num_of_courses = course_list[0][0]
 
     cons = []
@@ -25,26 +24,35 @@ def kenken_csp_model(course_list):
     # add all diff constraint for each course
     for i in range(0, len(vars)):
         sat_tuples = []
-        for j in range(i, len(vars)):
+        for j in range(i+1, len(vars)):
             sat_tuples = check_vars(vars[i].domain(), vars[j].domain())
 
-        con = Constraint("C(CS{},CS{})".format(i + 1, j + 1), [vars[i], vars[j]])
+            con = Constraint("C(CS{},CS{})".format(i + 1, j + 1), [vars[i], vars[j]])
+            con.add_satisfying_tuples(sat_tuples)
+            cons.append(con)
+
+    #add constraints for nogo times
+    for i in range(0, len(vars)):
+        sat_tuples = []
+        sat_list = []
+        for nogo_time in no_go:
+            for session in vars[i].domain():
+                if nogo_time not in session and session not in sat_list:
+                    sat_list.append(session)
+        sat_t = tuple(sat_list)
+        sat_tuples.append(sat_t)
+
+
+        con = con = Constraint("C(CS{},nogo)".format(i + 1,), [vars[i]])
         con.add_satisfying_tuples(sat_tuples)
         cons.append(con)
 
-    # add constraints for nogo times
-    # for var in vars:
-    #     sat_tuples = []
-    #     for nogo_time in no_go:
-    #         if no_go not in var.domain():
-    #             sat_tuples.append()
-
+    #make csp
     course_csp = CSP("course_csp")
 
     # add all vars
-    for row in vars:
-        for v in row:
-            course_csp.add_var(v)
+    for var in vars:
+        course_csp.add_var(var)
 
     # add all constraints
     for each_con in cons:
@@ -53,15 +61,12 @@ def kenken_csp_model(course_list):
     return course_csp, vars
 
 
-
-
-
 def check_vars(d0, d1):
     sat_tuples = []
 
 
     for t in itertools.product(d0, d1):
-        if t[0] != t[1]:
+        if (t[1] not in t[0]) and (t[0] not in t[1]):
             sat_tuples.append(t)
 
     return sat_tuples
