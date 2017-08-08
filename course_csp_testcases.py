167c13127e3d7c504515a4d2982ee31e00110970
williams <jcj0409@hotmail.com>
2017-08-08 16:15:24 -0400

    Testcase update & variable encode

--- course_csp_testcases.py
+++ course_csp_testcases.py
@@ -4,7 +4,11 @@ from orderings import *
 
 
 testcase = [
-    [[5],["M8,M9,M10","Tu13,Tu14,Tu15"],["Tu9,Tu10","Th11,Th12","F9,F10"],["F14,F15,F16"],["M10","Tu13","W11","W14"],["Th12,Th13,Th14","F8,F9,F10"],["Tu11","Tu13","W11","F8"]]
+    [[5],["M8,M9,M10","Tu13,Tu14,Tu15"],["Tu9,Tu10","Th11,Th12","F9,F10"],["F14,F15,F16"],["M10","Tu13","W11","W14"],["Th12,Th13,Th14","F8,F9,F10"],["Tu11","Tu13","W11","F8"]],
+    #no solution case 1
+    [[3],["M8,M9,M10"],["Tu8,Tu9,Tu10"],["W8,W9,W10"],["M9","Tu9","W9"]],
+    #no solution case 2
+    [[3],["M8,M9,M10","Th12,Th13,Th14"],["Tu8,Tu9,Tu10","Th14,Th15,Th16"],["W8,W9,W10","Th10,Th11,Th12"],["M9","Tu9","Wp"]]
 ]
 
 def print_course_soln(var_array):
